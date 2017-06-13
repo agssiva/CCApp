5695f65d95561db0f9eab6e26c5683a9fe276432
agssiva <agssiva@gmail.com>
2017-06-13 11:09:50 +0530

    API modified

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.get('/', function (request, response) {
     response.redirect('/agent');
 });
 
+app.get('/api/login', (req, res) => {
 	res.send({"message" : "Success"});
+});
 
 var server = http.createServer(app);
 var port = process.env.PORT || 1347;
